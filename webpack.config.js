811d417203736809e81e29a71b300a900c141835
brunningman <abrunner221@gmail.com>
2019-07-29 20:18:50 -0700

    I don't remember

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: {
-    vendor: ['styled-components'],
-    app1: `${SRC_DIR}/index.jsx`
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-    })
-  ],
+  entry:  `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
